6acb121e1c916ca5bb972166c9a733da1e8b0134
KirillKurril <kirillkurill1525@gmail.com>
2023-11-15 13:46:44 +0300

    ALWD 5 init

---
+++ ALWD5/ALWD5.asm
@@ -0,0 +1,32 @@
+include dmitruk_1.1.1.inc
+
+.data 
+    str1 db 0fdh, 254 dup('$')
+    str2 db 0fdh, 254 dup('$')   
+    
+.stack
+    dw 128 dup(0)
+    
+.code
+    start:     
+	    
+        data_init
+                 
+        get_string str1 
+        
+        print_new_line  
+         
+        get_string str2  
+           
+        space_check str2
+        cmp al, -1
+        je exp                         
+                   
+        remove_word str1, str2
+               
+exp:                   
+        print_new_line   
+               
+        print_string_as_chars str1
+
+end start
